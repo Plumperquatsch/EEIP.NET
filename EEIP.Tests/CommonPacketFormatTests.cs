4684a43b75a7382013141db60129fee9ec2fe0d7
Matthias Hofmayer <infom@hofmayer.name>
2020-04-18 18:36:25 +0200

    Add tests for CommonPacketFormat

--- EEIP.Tests/CommonPacketFormatTests.cs
+++ EEIP.Tests/CommonPacketFormatTests.cs
@@ -1,9 +1,12 @@
-﻿using EEIP.Tests;
+﻿using Sres.Net.EEIP.Tests;
 using NUnit.Framework;
 using Sres.Net.EEIP;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Runtime.InteropServices;
+using System.Buffers.Binary;
+using FluentAssertions;
 
 namespace Sres.Net.EEIP.Tests
 {
@@ -11,16 +14,60 @@ namespace Sres.Net.EEIP.Tests
     public class CommonPacketFormatTests
     {
         [Test()]
-        public void SerializeToBytesTest()
+        public void SerializePacketAsUnconnectedDataWithSocketAddressTest()
         {
 
             // Arrange
             var seed = TestHelper.CreateRandomSeed();
             var random = new Random(seed);
 
-            Span<byte> commonPacketFormatSpan = new Span<byte>();
+            var dataCount = (byte)random.Next(0, 255);
+            var testData = new byte[dataCount];
+            random.NextBytes(testData);
 
-            throw new NotImplementedException();
+            Encapsulation.SocketAddress sockAddress = new Encapsulation.SocketAddress(0x1234, 0x4567, 0x12345678);
+
+            var commonPacket = new Encapsulation.CommonPacketFormat()
+            {
+                DataLength = dataCount,
+                SocketaddrInfo_O_T = sockAddress
+            };
+            commonPacket.Data.AddRange(testData);
+
+            // Act
+            var commonPackteBytes = commonPacket.SerializeToBytes();
+
+            //Assert
+            Span<byte> commonPacketSpan = new Span<byte>(commonPackteBytes);
+            var itemCount = BinaryPrimitives.ReadUInt16LittleEndian(commonPacketSpan.Slice(0, 2));
+            var addressItem = BinaryPrimitives.ReadUInt16LittleEndian(commonPacketSpan.Slice(2, 2));
+            var addressLength = BinaryPrimitives.ReadUInt16LittleEndian(commonPacketSpan.Slice(4, 2));
+            
+            var dataItem = BinaryPrimitives.ReadUInt16LittleEndian(commonPacketSpan.Slice(6, 2));
+            var dataLength = BinaryPrimitives.ReadUInt16LittleEndian(commonPacketSpan.Slice(8, 2));
+            var data = commonPacketSpan.Slice(10, dataCount);
+
+            var sockaddrInfoItem = BinaryPrimitives.ReadUInt16LittleEndian(commonPacketSpan.Slice(10 + dataCount, 2));
+            var sockaddrInfoLength = BinaryPrimitives.ReadUInt16LittleEndian(commonPacketSpan.Slice(10 + dataCount + 2, 2));
+            var sinFamily = BinaryPrimitives.ReadUInt16BigEndian(commonPacketSpan.Slice(10 + dataCount + 4, 2));
+            var sinPort = BinaryPrimitives.ReadUInt16BigEndian(commonPacketSpan.Slice(10 + dataCount + 6, 2));
+            var sinAddr = BinaryPrimitives.ReadUInt32BigEndian(commonPacketSpan.Slice(10 + dataCount + 8, 4));
+            var sinZero = commonPacketSpan.Slice(10 + dataCount + 12, 8);
+
+            itemCount.Should().Be(commonPacket.ItemCount);
+            addressItem.Should().Be(commonPacket.AddressItem);
+            addressLength.Should().Be(commonPacket.AddressLength);
+
+            dataItem.Should().Be(commonPacket.DataItem);
+            dataLength.Should().Be(commonPacket.DataLength);
+            data.ToArray().Should().BeEquivalentTo(testData);
+            
+            sockaddrInfoItem.Should().Be(commonPacket.SockaddrInfoItem_O_T);
+            sockaddrInfoLength.Should().Be(commonPacket.SockaddrInfoLength);
+            sinFamily.Should().Be(commonPacket.SocketaddrInfo_O_T.SIN_family);
+            sinPort.Should().Be(commonPacket.SocketaddrInfo_O_T.SIN_port);
+            sinAddr.Should().Be(commonPacket.SocketaddrInfo_O_T.SIN_Address);
+            sinZero.ToArray().Should().BeEquivalentTo(commonPacket.SocketaddrInfo_O_T.SIN_Zero);
         }
     }
 }
