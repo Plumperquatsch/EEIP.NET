453c50a466f6fe67fa0260d8e1b16bc6226a2e19
Matthias Hofmayer <infom@hofmayer.name>
2020-05-02 15:28:50 +0200

    Implemented test for to retry RegisterSession

--- EEIP.Tests/RegisterSessionTests.cs
+++ EEIP.Tests/RegisterSessionTests.cs
@@ -4,6 +4,7 @@ using Sres.Net.EEIP;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Diagnostics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -24,20 +25,39 @@ namespace Sres.Net.EEIP.Tests
 
             // Act
             var sessionHandle = await eeipClient.RegisterSessionAsync(deviceUri);
+            
+            // Assert
+            sessionHandle.Should().Be(eeipDevice.SessionHandle);
+            eeipDevice.Connected.Should().BeTrue();
+            eeipDevice.SessionRegistered.Should().BeTrue();
         }
 
         [Test]
-        public async Task RegisterSessionTimeOut()
+        public async Task RetryRegisterSessionAfterTimeOutShallSucceed()
         {
             // Arrange
             var port = TestHelper.GetAvailablePort();
-            using var eeipDevice = new EeipDeviceMock(port);
             using EEIPClient eeipClient = new Sres.Net.EEIP.EEIPClient();
             Uri deviceUri = new UriBuilder("tcp", "localhost", port).Uri;
 
-            // Act
-            var sessionHandle = await eeipClient.RegisterSessionAsync(deviceUri);
+            uint sessionHandle;
+            try
+            {
+                sessionHandle = await eeipClient.RegisterSessionAsync(deviceUri);
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine($"First connection failed as intended: {ex.Message}");
+            }
 
+            // Act
+            using var eeipDevice = new EeipDeviceMock(port);
+            sessionHandle = await eeipClient.RegisterSessionAsync(deviceUri);
+            
+            // Assert
+            sessionHandle.Should().Be(eeipDevice.SessionHandle);
+            eeipDevice.Connected.Should().BeTrue();
+            eeipDevice.SessionRegistered.Should().BeTrue();
         }
     }
 }
